82bcc152a6f63b3a29f132c7f9004342ffc6a825
Your Name <your_email@example.com>
2025-10-30 08:44:59 +0000

    Add Naive Bayes classifier code and notebook

--- naive_bayes_classifier.py
+++ naive_bayes_classifier.py
@@ -4,58 +4,44 @@ import pandas as pd
 
 class NaiveBayesClassifier:
     def __init__(self):
-        self.prior_probs = {}
-        self.conditional_probs = {}
+        self.class_priors = {}
+        self.feature_means = {}
+        self.feature_variances = {}
         self.classes = None
-        self.features = None
 
-    def calculate_prior_probs(self, df):
-        total_samples = len(df)
-        self.classes = df['species'].unique()
-        for cls in self.classes:
-            class_samples = len(df[df['species'] == cls])
-            self.prior_probs[cls] = class_samples / total_samples
-
-    def calculate_conditional_probs(self, df):
-        self.features = df.columns.tolist()
-        self.features.remove('species')
+    def fit(self, X_train, y_train):
+        self.classes = np.unique(y_train)
+        n_samples, n_features = X_train.shape
 
         for cls in self.classes:
-            self.conditional_probs[cls] = {}
-            class_df = df[df['species'] == cls]
-            for feature in self.features:
-                mean = class_df[feature].mean()
-                std = class_df[feature].std()
-                self.conditional_probs[cls][feature] = {'mean': mean, 'std': std}
+            X_cls = X_train[y_train == cls]
+            self.class_priors[cls] = len(X_cls) / n_samples
+            self.feature_means[cls] = X_cls.mean(axis=0)
+            self.feature_variances[cls] = X_cls.var(axis=0)
 
-    def fit(self, df):
-        self.calculate_prior_probs(df)
-        self.calculate_conditional_probs(df)
+    def _gaussian_probability(self, x, mean, variance):
+        # Add a small epsilon to variance to avoid division by zero
+        variance = variance + 1e-6
+        exponent = -((x - mean) ** 2) / (2 * variance)
+        numerator = np.exp(exponent)
+        denominator = np.sqrt(2 * np.pi * variance)
+        return numerator / denominator
 
-    def predict_proba(self, sample):
-        posteriors = {}
-        for cls in self.classes:
-            posterior = self.prior_probs[cls]
-            for feature in self.features:
-                mean = self.conditional_probs[cls][feature]['mean']
-                std = self.conditional_probs[cls][feature]['std']
+    def predict_proba(self, X_test):
+        n_samples = X_test.shape[0]
+        n_classes = len(self.classes)
+        probabilities = np.zeros((n_samples, n_classes))
 
-                # Handle zero standard deviation
-                if std == 0:
-                    std = 1e-9  # Add a small smoothing value
+        for i, cls in enumerate(self.classes):
+            prior = self.class_priors[cls]
+            likelihood = np.prod(self._gaussian_probability(X_test, self.feature_means[cls], self.feature_variances[cls]), axis=1)
+            probabilities[:, i] = prior * likelihood
 
-                exponent = -((sample[feature] - mean) ** 2) / (2 * (std ** 2))
-                # Add a small value to probability_density to avoid log(0) if using log probabilities
-                probability_density = (1 / (np.sqrt(2 * np.pi) * std)) * np.exp(exponent)
-                posterior *= probability_density # Consider using log probabilities to avoid underflow
+        # Normalize probabilities
+        row_sums = probabilities.sum(axis=1)[:, np.newaxis]
+        probabilities = probabilities / row_sums
+        return probabilities
 
-            posteriors[cls] = posterior
-        return posteriors
-
-    def predict(self, df_test):
-        predictions = []
-        for index, row in df_test.iterrows():
-            posteriors = self.predict_proba(row)
-            predicted_class = max(posteriors, key=posteriors.get)
-            predictions.append(predicted_class)
-        return predictions
+    def predict(self, X_test):
+        probabilities = self.predict_proba(X_test)
+        return self.classes[np.argmax(probabilities, axis=1)]
